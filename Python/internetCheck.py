c6d9b40b4da66df6ec33599a0c5c4463cfbc64b8
Riccardo Rossi <31690722+Riccardo02@users.noreply.github.com>
2020-01-14 22:13:45 +0100

    Delete internetCheck.py

--- Python/internetCheck.py
+++
@@ -1,9 +0,0 @@
-def have_internet():
-	conn = httplib.HTTPConnection("www.google.com", timeout=5)
-	try:
-		conn.request("HEAD", "/")
-		conn.close()
-		return True
-	except:
-		conn.close()
-		return False
