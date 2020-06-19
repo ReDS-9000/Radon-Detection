5e8c3c108e7181a2de80019f7235258a4e9f214e
Riccardo Rossi <31690722+ReDS-9000@users.noreply.github.com>
2020-06-19 17:43:15 +0200

    Added getfile

---
+++ Database_Side/getfile.py
@@ -0,0 +1,45 @@
+
+import imaplib, email, os, json
+
+
+contatore=json.load(open("contatore.json","r"))
+
+detach_dir = ''
+
+m = imaplib.IMAP4_SSL("imap.gmail.com")
+m.login('radonthrowaway@gmail.com', 'Radon2020!')
+
+m.select("inbox")
+
+resp, items = m.search(None, "(UNSEEN)")
+items = items[0].split()
+
+for emailid in items:
+    resp, data = m.fetch(emailid, "(RFC822)")
+    print(data)
+    email_body = data[0][1]
+    mail = email.message_from_string(email_body)
+    temp = m.store(emailid, '+FLAGS', '\\Seen')
+    m.expunge()
+
+    if mail.get_content_maintype() != 'multipart':
+        continue
+
+
+    if mail["From"] == 'radonthrowaway@gmail.com':
+        for part in mail.walk():
+            if part.get_content_maintype() == 'multipart':
+                continue
+            if part.get('Content-Disposition') is None:
+                continue
+
+            filename = part.get_filename()
+            att_path = detach_dir + str(filename)
+
+            if not os.path.isfile(att_path) :
+                fp = open("/nuoviDati/nuovoDato"+str(contatore["contatore"])+".json", 'wb')
+                fp.write(part.get_payload(decode=True))
+                fp.close()
+                contatore["contatore"]+=1
+                with open("contatore.json","w") as outfile:
+                    json.dump(contatore,outfile,indent=8)
